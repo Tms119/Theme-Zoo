2dee7bc280620fbada2eada4ba0304bf87d6cd12
sayem <sayem@TMSs-MacBook-Pro.local>
2026-06-22 03:06:01 +0600

    Feat: Add Tawk.to live chat widget

--- app/layout.js
+++ app/layout.js
@@ -131,6 +131,21 @@ export default function RootLayout({ children }) {
           `}
         </Script>
 
+        {/* Tawk.to Live Chat */}
+        <Script id="tawk-to" strategy="lazyOnload">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6a36dd1c2e5a421d56c2fa57/1jrj4veoh';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
+
         <ClerkProvider
           appearance={{
             baseTheme: dark,
